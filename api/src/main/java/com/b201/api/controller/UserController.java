5fec69bbe2d0058c5a466401b499b6dcfaff669d
김형표 <jmn9798@naver.com>
2025-04-30 17:21:41 +0900

    ✨ feat: 회원가입 기능 구현

---
+++ api/src/main/java/com/b201/api/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.b201.api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.b201.api.dto.SignupDto;
+import com.b201.api.service.UserService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+
+	private final UserService userService;
+
+	@PostMapping("/signup")
+	public ResponseEntity<?> signup(@Valid @RequestBody SignupDto signupDto) {
+		userService.signUp(signupDto);
+		return ResponseEntity.ok().build();
+	}
+}
---
+++ api/src/main/java/com/b201/api/dto/LoginDto.java
@@ -0,0 +1,10 @@
+package com.b201.api.dto;
+
+import lombok.Getter;
+
+@Getter
+public class LoginDto {
+
+	private String username;
+	private String password;
+}
