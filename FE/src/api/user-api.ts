51aa33258c4785ebd2fd66779b0dd96ed1ff3f89
enderstream00 <enderstream00@naver.com>
2025-05-08 10:33:28 +0900

    ✨ feat: user api 구현 중

--- FE/src/api/user-api.ts
+++ FE/src/api/user-api.ts
@@ -1,3 +1,41 @@
 import apiClient from "@/api/api-client"
+import { LoginFormData, RegisterFormData } from "@/types/user"
 
-// user api는 나중에 백엔드 api 가 나오면 구현 예정
+export const userAPI = {
+    login: async (Form: LoginFormData) => {
+        try {
+            const response = await apiClient.post("/api/users/login", { id: Form.id, password: Form.password })
+            return { data: response.data, status: response.status }
+        } catch (error) {
+            console.error(`로그인에 실패하였습니다`)
+            throw error
+        }
+    },
+    logout: async () => {
+        try {
+            const response = await apiClient.post("/api/users/logout")
+            return { data: response.data, status: response.status }
+        } catch (error) {
+            console.error(`로그아웃에 실패하였습니다`)
+            throw error
+        }
+    },
+    register: async (Form: RegisterFormData) => {
+        try {
+            const response = await apiClient.post("/api/users/signup", { id: Form.id, password: Form.password, name: Form.name, regionId: Form.id })
+            return { data: response.data, status: response.status }
+        } catch (error) {
+            console.error(`회원가입에 실패하였습니다`)
+            throw error
+        }
+    },
+    refresh: async () => {
+        try {
+            const response = await apiClient.post("/api/users/refresh")
+            return { data: response.data, status: response.status }
+        } catch (error) {
+            console.error(`리프레시 토큰 발급에 실패하였습니다`)
+            throw error
+        }
+    }
+}
