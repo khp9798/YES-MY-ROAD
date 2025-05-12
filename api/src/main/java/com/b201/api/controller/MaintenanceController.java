25bd715879836d9f7fca25109941470d277c9e6a
김형표 <jmn9798@naver.com>
2025-05-12 15:15:28 +0900

    ✨ feat: 월별 보수공사 현황 기능 구현

--- api/src/main/java/com/b201/api/controller/MaintenanceController.java
+++ api/src/main/java/com/b201/api/controller/MaintenanceController.java
@@ -1,5 +1,7 @@
 package com.b201.api.controller;
 
+import java.util.List;
+
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.b201.api.dto.maintenance.CompletionStatsDto;
 import com.b201.api.dto.maintenance.MaintenanceStatusDto;
+import com.b201.api.dto.maintenance.MonthlyMaintenanceStatusDto;
 import com.b201.api.service.MaintenanceService;
 
 import lombok.RequiredArgsConstructor;
@@ -30,5 +33,10 @@ public class MaintenanceController {
 	public ResponseEntity<CompletionStatsDto> getCompletionStats() {
 		log.info("result: {}", maintenanceService.getCompletionStats());
 		return ResponseEntity.ok().body(maintenanceService.getCompletionStats());
+	}
+
+	@GetMapping("/monthly-status")
+	public ResponseEntity<List<MonthlyMaintenanceStatusDto>> getMonthlyStats() {
+		return ResponseEntity.ok().body(maintenanceService.getMonthlyMaintenanceStatus());
 	}
 }
--- api/src/main/java/com/b201/api/service/MaintenanceService.java
+++ api/src/main/java/com/b201/api/service/MaintenanceService.java
@@ -1,11 +1,13 @@
 package com.b201.api.service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.b201.api.dto.maintenance.CompletionStatsDto;
 import com.b201.api.dto.maintenance.MaintenanceStatusDto;
+import com.b201.api.dto.maintenance.MonthlyMaintenanceStatusDto;
 import com.b201.api.repository.CaptureDamageRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -27,5 +29,9 @@ public class MaintenanceService {
 			now.minusWeeks(1),
 			now.minusMonths(1)
 		);
+	}
+
+	public List<MonthlyMaintenanceStatusDto> getMonthlyMaintenanceStatus() {
+		return captureDamageRepository.getMonthlyMaintenanceStatsByPeriod();
 	}
 }
