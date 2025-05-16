665a85bd62b151208bf7379a8441896c4367c3ab
김형표 <jmn9798@naver.com>
2025-05-16 11:16:20 +0900

    ✨ feat: 유지보수 현황 페이지 사용자 지역 권한에 따른 필터링

--- api/src/main/java/com/b201/api/service/MaintenanceService.java
+++ api/src/main/java/com/b201/api/service/MaintenanceService.java
@@ -21,17 +21,18 @@ public class MaintenanceService {
 
 	private final CaptureDamageRepository captureDamageRepository;
 
-	public MaintenanceStatusDto getMaintenanceStatus() {
+	public MaintenanceStatusDto getMaintenanceStatus(String regionName) {
 		log.info("[getMaintenanceStatus] 호출됨");
-		MaintenanceStatusDto status = captureDamageRepository.countByStauts();
+		MaintenanceStatusDto status = captureDamageRepository.countByStauts(regionName);
 		log.debug("[getMaintenanceStatus] result = {}", status);
 		return status;
 	}
 
-	public CompletionStatsDto getCompletionStats() {
+	public CompletionStatsDto getCompletionStats(String regionName) {
 		log.info("[getCompletionStats] 호출됨");
 		LocalDateTime now = LocalDateTime.now();
 		CompletionStatsDto stats = captureDamageRepository.getCompletionStatsByPeriod(
+			regionName,
 			now.minusDays(1),
 			now.minusWeeks(1),
 			now.minusMonths(1)
@@ -40,16 +41,16 @@ public class MaintenanceService {
 		return stats;
 	}
 
-	public List<MonthlyMaintenanceStatusDto> getMonthlyMaintenanceStatus() {
+	public List<MonthlyMaintenanceStatusDto> getMonthlyMaintenanceStatus(String regionName) {
 		log.info("[getMonthlyMaintenanceStatus] 호출됨");
-		List<MonthlyMaintenanceStatusDto> list = captureDamageRepository.getMonthlyMaintenanceStatsByPeriod();
+		List<MonthlyMaintenanceStatusDto> list = captureDamageRepository.getMonthlyMaintenanceStatsByPeriod(regionName);
 		log.debug("[getMonthlyMaintenanceStatus] entries = {}", list.size());
 		return list;
 	}
 
-	public List<RegionMaintenanceStatusDto> getRegionMaintenanceStatus() {
+	public List<RegionMaintenanceStatusDto> getRegionMaintenanceStatus(String regionName) {
 		log.info("[getRegionMaintenanceStatus] 호출됨");
-		List<RegionMaintenanceStatusDto> list = captureDamageRepository.getRegionMaintenanceStatsByPeriod("대전광역시");
+		List<RegionMaintenanceStatusDto> list = captureDamageRepository.getRegionMaintenanceStatsByPeriod(regionName);
 		log.debug("[getRegionMaintenanceStatus] entries = {}", list.size());
 		return list;
 	}
