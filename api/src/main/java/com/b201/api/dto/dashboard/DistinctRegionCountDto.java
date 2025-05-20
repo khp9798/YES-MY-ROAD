5509ab5a0d90fbe978e45856c2b938e563f02457
김형표 <jmn9798@naver.com>
2025-05-20 15:31:06 +0900

    ✨ feat: 도로 파손이 감지된 주소에서 중복을 제거하여 개수를 반환 로직

---
+++ api/src/main/java/com/b201/api/dto/dashboard/DistinctRegionCountDto.java
@@ -0,0 +1,14 @@
+package com.b201.api.dto.dashboard;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@AllArgsConstructor
+public class DistinctRegionCountDto {
+
+	private long regionCount;
+	
+}
